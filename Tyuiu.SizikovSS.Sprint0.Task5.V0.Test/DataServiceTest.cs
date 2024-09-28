5c8a0b4258d982190b0ee02086b43fb2dbdff708
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-09-28 14:59:29 +0500

    Task5

---
+++ Tyuiu.SizikovSS.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SizikovSS.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.SizikovSS.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
+    }
+}
