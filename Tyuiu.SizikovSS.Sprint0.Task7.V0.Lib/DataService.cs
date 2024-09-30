3081ed96645ea6fa5a7409214e1fa7fbdd8061df
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-10-01 01:52:50 +0500

    Task7

---
+++ Tyuiu.SizikovSS.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.SizikovSS.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resulrArray = new int[5];
+            for (var i = 0; i < resulrArray.Length; i++)
+            {
+                resulrArray[i] = numOne[i] + numTwo[i];
+            }
+            return resulrArray;
+        }
+    }
+}
